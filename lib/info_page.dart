332ba4017dc9e7f67ced79f4e992928f8b7eeaa6
Z-oybek <newprogrammer2023@gmail.com>
2024-04-17 00:08:11 +0500

    info page updated

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 import 'package:identify/class.dart';
 
 class InfoPage extends StatefulWidget {
@@ -19,48 +20,114 @@ class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff333355),
+      backgroundColor: const Color(0xff333355),
       appBar: AppBar(centerTitle: true, title: const Text("My Client")),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                kIsWeb
+                    ? Center(child: FlutterLogo(size: MediaQuery.of(context).size.width * 0.45))
+                    : Center(
+                        child: Image.file(
+                          widget.identify.image ?? File(""),
+                          width: MediaQuery.of(context).size.width * 0.04,
+                          height: MediaQuery.of(context).size.height * 0.04,
+                          fit: BoxFit.fill,
+                          filterQuality: FilterQuality.high,
+                        ),
+                      ),
+                const Gap(10),
+                Table(
+                  border: TableBorder.all(width: 2, color: CupertinoColors.systemGrey),
                   children: [
-                    Text("Lavozimi: ${widget.identify.position ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Familiyasi: ${widget.identify.surname ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Ismi: ${widget.identify.name ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Sharifi: ${widget.identify.parentalName ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Ilmiy Darajasi: ${widget.identify.eduPosition ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Telefon Raqami: ${widget.identify.phoneNumber ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Elektron Pochtasi: ${widget.identify.email ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
-                    Text("Qabul Kunlari: ${widget.identify.approvalDays ?? ""}",
-                        style: const TextStyle(fontSize: 20, color: CupertinoColors.white)),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                            child:
+                                Text("Familiyasi", style: TextStyle(color: CupertinoColors.black)),
+                          ),
+                          TableCell(
+                            child: Text(widget.identify.surname ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                            child: Text("Ismi", style: TextStyle(color: CupertinoColors.black)),
+                          ),
+                          TableCell(
+                            child: Text(widget.identify.name ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                              child:
+                                  Text("Sharifi", style: TextStyle(color: CupertinoColors.black))),
+                          TableCell(
+                            child: Text(widget.identify.parentalName ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                              child: Text("Ilmiy Darajasi",
+                                  style: TextStyle(color: CupertinoColors.black))),
+                          TableCell(
+                            child: Text(widget.identify.eduPosition ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                              child: Text("Telefon Raqami",
+                                  style: TextStyle(color: CupertinoColors.black))),
+                          TableCell(
+                            child: Text(widget.identify.phoneNumber ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                              child: Text("Elektron Pochtasi",
+                                  style: TextStyle(color: CupertinoColors.black))),
+                          TableCell(
+                            child: Text(widget.identify.email ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
+                    TableRow(
+                        decoration: const BoxDecoration(color: CupertinoColors.white),
+                        children: [
+                          const TableCell(
+                            child: Text("Qabul Kunlari",
+                                style: TextStyle(color: CupertinoColors.black)),
+                          ),
+                          TableCell(
+                            child: Text(widget.identify.approvalDays ?? "",
+                                style: const TextStyle(color: CupertinoColors.black)),
+                          ),
+                        ]),
                   ],
                 ),
-              ),
-              kIsWeb
-                  ? FlutterLogo(size: MediaQuery.of(context).size.width * 0.45)
-                  : Image.file(
-                      widget.identify.image ?? File(""),
-                      width: MediaQuery.of(context).size.width * 0.04,
-                      height: MediaQuery.of(context).size.height * 0.04,
-                      fit: BoxFit.fill,
-                      filterQuality: FilterQuality.high,
-                    )
-            ],
+              ],
+            ),
           ),
         ),
       ),
